bc7037424a886728654e68d62ab103a28abe0083
Will Gallop <wgallop99@gmail.com>
2014-09-12 14:54:33 -0400

    reset

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,23 @@ angular
         templateUrl: 'views/main.html',
         controller: 'PostsCtrl'
       })
-      .when('/chat', {
+      .when('/blog', {
         templateUrl: 'views/blog-list.html',
         controller: 'PostsCtrl'
+      })
+      // .when('/new', {
+      //   templateUrl: 'views/blog-create.html',
+      //   controller: 'PostsCtrl'
+      // })
+      // .when('/blog/:id', {
+      //   templateUrl: 'views/blog-detail.html',
+      //   controller: 'PostCtrl'
+      // })
+      // .when('/blog/:id/edit', {
+      //   templateUrl: 'views/blog-editDetail.html',
+      //   controller: 'PostCtrl'
+      // })
+      .otherwise({
+        redirectTo: '/'
+      });
   });
